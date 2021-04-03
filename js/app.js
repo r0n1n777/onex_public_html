093ade1d3c15ee9e18acd49bfdbe0f8db1924a8a
Ero Aguilar <eroaguilar7@gmail.com>
2021-04-04 04:13:40 +0800

    Activation functionality done.

--- js/app.js
+++ js/app.js
@@ -1841,11 +1841,11 @@ module.exports = {
 /***/ (() => {
 
 $('.confirmation').on("click", function () {
-  $('#id').val($(this).attr('id'));
-  $('#name').html($(this).attr('name'));
-  $('#phone').html($(this).attr('phone'));
-  $('#email').html($(this).attr('email'));
-  $('#date').html($(this).attr('date'));
+  $('.id').val($(this).attr('id'));
+  $('.name').html($(this).attr('name'));
+  $('.phone').html($(this).attr('phone'));
+  $('.email').html($(this).attr('email'));
+  $('.date').html($(this).attr('date'));
 });
 
 /***/ }),
